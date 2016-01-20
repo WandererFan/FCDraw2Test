31da160c359de61bc312d788ff0de01acfec2e42
WandererFan <wandererfan@gmail.com>
2016-01-20 12:14:17 -0500

    add a basic test script

---
+++ Scripts/basicTest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# basic test script for TechDraw module
+# assumes an active empty document to start
+from __future__ import print_function
+
+import FreeCAD
+import Part
+import Measure
+import TechDraw
+
+templateFileSpec = '/home/cheinz/freecad-draw2-build/data/Mod/Drawing/Templates/A4_Landscape.svg'
+
+print("basic test started")
+box = FreeCAD.ActiveDocument.addObject("Part::Box","Box")
+
+FreeCAD.ActiveDocument.addObject('TechDraw::DrawPage','Page')
+FreeCAD.ActiveDocument.addObject('TechDraw::DrawSVGTemplate','Template')
+FreeCAD.ActiveDocument.Template.Template = templateFileSpec
+FreeCAD.ActiveDocument.Page.Template = FreeCAD.ActiveDocument.Template
+
+view = FreeCAD.ActiveDocument.addObject('TechDraw::DrawViewPart','View')
+# need another Py command here to add View to Page
+FreeCAD.ActiveDocument.View.Source = App.ActiveDocument.Box
+FreeCAD.ActiveDocument.View.Direction = (0.0,0.0,1.0)
+FreeCAD.ActiveDocument.View.X = 10.0
+FreeCAD.ActiveDocument.View.Y = 10.0
+FreeCAD.ActiveDocument.View.Scale = 1.0
+FreeCAD.ActiveDocument.View.Rotation = 0.0
+FreeCAD.ActiveDocument.recompute()
+
+print("basic test ended")
